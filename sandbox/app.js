45adcb97bde9f1d5be8fd42e37aa856821150fea
Alejandro Rios <Alx.Rios91@gmail.com>
2018-08-31 23:03:48 -0500

    updated sandbox

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,10 +1,33 @@
-for(var x=1; x <= 20; x++){
-    var button = document.createElement('button');
-    button.innerHTML= x;
-    button.className = "button"
-    document.body.appendChild(button);
+//person constructor
+function Person(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
 }
 
-document.querySelectorAll('.button').forEach(button => {
-    button.onclick = () => alert(button.innerHTML);
-});
+//greeting
+Person.prototype.greeting = function(){
+    return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+
+//customer constructor
+function Customer(firstName, lastName, phone, membership){
+    Person.call(this, firstName, lastName);
+    this.phone = phone;
+    this.membership = membership;
+}
+
+//inherit person prototype
+Customer.prototype = Object.create(Person.prototype);
+//make customer.prototype return Customer
+Customer.prototype.constructor = Customer;
+
+const person1 = new Person('Alex', 'Rios');
+const customer1 = new Customer('Alex', 'Rios', '956-529-6849', 'Admin');
+
+console.log(person1.greeting());
+
+Customer.prototype.greeting = function(){
+    return `Hello there ${this.firstName} ${this.lastName}, welcome to our company`;
+}
+console.log(customer1.Person.greeting());
